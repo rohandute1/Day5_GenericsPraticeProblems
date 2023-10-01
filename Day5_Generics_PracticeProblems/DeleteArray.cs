e8e739181e26d691845a65d896cba8bc033fa111
Rohan <rohandute1@gmail.com>
2023-10-01 13:38:31 +0530

    [rohan] Deleting array element using normal method

---
+++ Day5_Generics_PracticeProblems/DeleteArray.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_Generics_PracticeProblems
+{
+    class DeleteArray
+    {
+        public static int[] DeleteMethod(int[] array, int elementToDelete)
+        {
+            int count = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == elementToDelete)
+                {
+                    count++;
+                }
+                else if (count > 0)
+                {
+                    array[i - count] = array[i];
+                }
+            }
+            int[] newArray = new int[array.Length - count];
+            Array.Copy(array, newArray, newArray.Length);
+            return newArray;
+        }
+
+        public static double[] DeleteMethod(double[] array, double elementToDelete)
+        {
+            int count = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == elementToDelete)
+                {
+                    count++;
+                }
+                else if (count > 0)
+                {
+                    array[i - count] = array[i];
+                }
+            }
+
+            double[] newArray = new double[array.Length - count];
+            Array.Copy(array, newArray, newArray.Length);
+            return newArray;
+        }
+
+        public static char[] DeleteMethod(char[] array, char elementToDelete)
+        {
+            int count = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == elementToDelete)
+                {
+                    count++;
+                }
+                else if (count > 0)
+                {
+                    array[i - count] = array[i];
+                }
+            }
+
+            char[] newArray = new char[array.Length - count];
+            Array.Copy(array, newArray, newArray.Length);
+            return newArray;
+        }
+    }
+}
--- Day5_Generics_PracticeProblems/Program.cs
+++ Day5_Generics_PracticeProblems/Program.cs
@@ -10,7 +10,76 @@ namespace Day5_Generics_PracticeProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Generics Problems.");   
+            Console.WriteLine("Welcome to Generics Problems.");
+            Console.WriteLine("Please select from Integer/Double/Char:");
+            Console.WriteLine("1.Integer Array\n2.Double Array\n3.Char Array");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the number of elements in the array:");
+            int n = int.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+
+                    int[] intArray = new int[n];
+
+                    Console.WriteLine("Enter the integer array:");
+                    for (int i = 0; i < n; i++)
+                    {
+                        intArray[i] = int.Parse(Console.ReadLine());
+                    }
+
+                    Console.WriteLine("Enter the element to delete from integer array:");
+                    int elementToDeleteInt = int.Parse(Console.ReadLine());
+                    int[] updatedIntArray = DeleteArray.DeleteMethod(intArray, elementToDeleteInt);
+
+                    Console.WriteLine("Updated Integer Array:");
+                    foreach (var item in updatedIntArray)
+                    {
+                        Console.Write(item + " ");
+                    }
+                    break;
+
+                case 2:
+                    double[] doubleArray = new double[n];
+                    Console.WriteLine("Enter the double array:");
+                    for (int i = 0; i < n; i++)
+                    {
+                        doubleArray[i] = double.Parse(Console.ReadLine());
+                    }
+
+                    Console.WriteLine("Enter the element to delete from double array:");
+                    double elementToDeleteDouble = double.Parse(Console.ReadLine());
+                    double[] updatedDoubleArray = DeleteArray.DeleteMethod(doubleArray, elementToDeleteDouble);
+
+                    Console.WriteLine("\nUpdated Double Array:");
+                    foreach (var item in updatedDoubleArray)
+                    {
+                        Console.Write(item + " ");
+                    }
+                    break;
+
+                case 3:
+                    char[] charArray = new char[n];
+                    Console.WriteLine("Enter the character array:");
+                    for (int i = 0; i < n; i++)
+                    {
+                        charArray[i] = char.Parse(Console.ReadLine());
+                    }
+
+                    Console.WriteLine("Enter the element to delete from character array:");
+                    char elementToDeleteChar = char.Parse(Console.ReadLine());
+                    char[] updatedCharArray = DeleteArray.DeleteMethod(charArray, elementToDeleteChar);
+
+                    Console.WriteLine("\nUpdated Character Array:");
+                    foreach (var item in updatedCharArray)
+                    {
+                        Console.Write(item + " ");
+                    }
+                    break;
+            }
+
             Console.ReadLine();
         }
     }
