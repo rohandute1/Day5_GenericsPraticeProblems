fc0e44b8a9e33687f98114a6bfac1b38f618da43
Rohan <rohandute1@gmail.com>
2023-10-01 16:06:18 +0530

    [rohan] find the maximum of three integers using CompareTo method

--- Day5_Generics_PracticeProblems/Program.cs
+++ Day5_Generics_PracticeProblems/Program.cs
@@ -11,78 +11,102 @@ namespace Day5_Generics_PracticeProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Generics Problems.");
-            Console.WriteLine("Please select from Integer/Double/Char:");
-            Console.WriteLine("1.Integer Array\n2.Double Array\n3.Char Array");
+            Console.WriteLine("Please choose program to perform:");
+            Console.WriteLine("1.Delete array element using generics or non generics\n2.Find Minimum of 3 integers");
+            int select = Convert.ToInt32(Console.ReadLine());
 
-            int option = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter the number of elements in the array:");
-            int n = int.Parse(Console.ReadLine());
-            switch (option)
+            switch (select)
             {
                 case 1:
 
-                    int[] intArray = new int[n];
+                    Console.WriteLine("Please select from Integer/Double/Char:");
+                    Console.WriteLine("1.Integer Array\n2.Double Array\n3.Char Array");
 
-                    Console.WriteLine("Enter the integer array:");
-                    for (int i = 0; i < n; i++)
+                    int option = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Enter the number of elements in the array:");
+                    int n = int.Parse(Console.ReadLine());
+                    switch (option)
                     {
-                        intArray[i] = int.Parse(Console.ReadLine());
-                    }
+                        case 1:
 
-                    Console.WriteLine("Enter the element to delete from integer array:");
-                    int elementToDeleteInt = int.Parse(Console.ReadLine());
-                    DeleteArray<int> intDeleteArray = new DeleteArray<int>();
-                    int[] updatedIntArray = intDeleteArray.DeleteMethod(intArray, elementToDeleteInt);
+                            int[] intArray = new int[n];
 
-                    Console.WriteLine("Updated Integer Array:");
-                    foreach (var item in updatedIntArray)
-                    {
-                        Console.Write(item + " ");
+                            Console.WriteLine("Enter the integer array:");
+                            for (int i = 0; i < n; i++)
+                            {
+                                intArray[i] = int.Parse(Console.ReadLine());
+                            }
+
+                            Console.WriteLine("Enter the element to delete from integer array:");
+                            int elementToDeleteInt = int.Parse(Console.ReadLine());
+                            DeleteArray<int> intDeleteArray = new DeleteArray<int>();
+                            int[] updatedIntArray = intDeleteArray.DeleteMethod(intArray, elementToDeleteInt);
+
+                            Console.WriteLine("Updated Integer Array:");
+                            foreach (var item in updatedIntArray)
+                            {
+                                Console.Write(item + " ");
+                            }
+                            break;
+
+                        case 2:
+                            double[] doubleArray = new double[n];
+                            Console.WriteLine("Enter the double array:");
+                            for (int i = 0; i < n; i++)
+                            {
+                                doubleArray[i] = double.Parse(Console.ReadLine());
+                            }
+
+                            Console.WriteLine("Enter the element to delete from double array:");
+                            double elementToDeleteDouble = double.Parse(Console.ReadLine());
+                            DeleteArray<double> doubleDeleteArray = new DeleteArray<double>();
+                            double[] updatedDoubleArray = doubleDeleteArray.DeleteMethod(doubleArray, elementToDeleteDouble);
+
+                            Console.WriteLine("\nUpdated Double Array:");
+                            foreach (var item in updatedDoubleArray)
+                            {
+                                Console.Write(item + " ");
+                            }
+                            break;
+
+                        case 3:
+                            char[] charArray = new char[n];
+                            Console.WriteLine("Enter the character array:");
+                            for (int i = 0; i < n; i++)
+                            {
+                                charArray[i] = char.Parse(Console.ReadLine());
+                            }
+
+                            Console.WriteLine("Enter the element to delete from character array:");
+                            char elementToDeleteChar = char.Parse(Console.ReadLine());
+                            DeleteArray<char> charDeleteArray = new DeleteArray<char>();
+                            char[] updatedCharArray = charDeleteArray.DeleteMethod(charArray, elementToDeleteChar);
+
+                            Console.WriteLine("\nUpdated Character Array:");
+                            foreach (var item in updatedCharArray)
+                            {
+                                Console.Write(item + " ");
+                            }
+                            break;
                     }
                     break;
 
                 case 2:
-                    double[] doubleArray = new double[n];
-                    Console.WriteLine("Enter the double array:");
-                    for (int i = 0; i < n; i++)
-                    {
-                        doubleArray[i] = double.Parse(Console.ReadLine());
-                    }
 
-                    Console.WriteLine("Enter the element to delete from double array:");
-                    double elementToDeleteDouble = double.Parse(Console.ReadLine());
-                    DeleteArray<double> doubleDeleteArray = new DeleteArray<double>();
-                    double[] updatedDoubleArray = doubleDeleteArray.DeleteMethod(doubleArray, elementToDeleteDouble);
+                    Console.WriteLine("Enter three integers:");
 
-                    Console.WriteLine("\nUpdated Double Array:");
-                    foreach (var item in updatedDoubleArray)
-                    {
-                        Console.Write(item + " ");
-                    }
+                    int num1 = int.Parse(Console.ReadLine());
+                    int num2 = int.Parse(Console.ReadLine());
+                    int num3 = int.Parse(Console.ReadLine());
+
+                    FindMinOfThree minFinder = new FindMinOfThree();
+                    int min = minFinder.FindMin(num1, num2, num3);
+
+                    Console.WriteLine("The minimum number is: " + min);
                     break;
 
-                case 3:
-                    char[] charArray = new char[n];
-                    Console.WriteLine("Enter the character array:");
-                    for (int i = 0; i < n; i++)
-                    {
-                        charArray[i] = char.Parse(Console.ReadLine());
-                    }
-
-                    Console.WriteLine("Enter the element to delete from character array:");
-                    char elementToDeleteChar = char.Parse(Console.ReadLine());
-                    DeleteArray<char> charDeleteArray = new DeleteArray<char>();
-                    char[] updatedCharArray = charDeleteArray.DeleteMethod(charArray, elementToDeleteChar);
-
-                    Console.WriteLine("\nUpdated Character Array:");
-                    foreach (var item in updatedCharArray)
-                    {
-                        Console.Write(item + " ");
-                    }
-                    break;
             }
-
             Console.ReadLine();
         }
     }
